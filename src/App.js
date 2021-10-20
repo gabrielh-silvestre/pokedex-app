6ba2172008cc57d9297f527083fe274abb673643
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-19 22:02:23 -0300

    importing PokemonCard component in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
+import PokeCard from './components/PokemonCard';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PokeCard
+        pokemonName="ditto"
+        pokemonId={132}
+        pokemonSprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+        pokemonTypes={['normal']}
+      />
+      <PokeCard
+        pokemonName="ditto"
+        pokemonId={132}
+        pokemonSprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+        pokemonTypes={['normal']}
+      />
+    </div>
+  );
 }
 
 export default App;
