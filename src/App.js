edc1d6a3533aaf35183e037b6ec370bc97e117ba
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-19 22:04:13 -0300

    change second ditto to squirtle

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
         pokemonTypes={['normal']}
       />
       <PokeCard
-        pokemonName="ditto"
-        pokemonId={132}
-        pokemonSprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-        pokemonTypes={['normal']}
+        pokemonName="squirtle"
+        pokemonId={7}
+        pokemonSprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
+        pokemonTypes={['water']}
       />
     </div>
   );
