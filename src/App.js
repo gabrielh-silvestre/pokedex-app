a2af70c080ceb246af7b0612074451e2369850f3
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-21 23:40:07 -0300

    refactor: delete PokeCards test component and importing Container

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import './App.css';
-import PokeCard from './components/PokemonCard';
+import Container from './components/Container/Container';
 
 function App() {
   return (
     <div className="App">
-      <PokeCard
-        pokemonName="ditto"
-        pokemonId={132}
-        pokemonSprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-        pokemonTypes={['normal']}
-      />
-      <PokeCard
-        pokemonName="squirtle"
-        pokemonId={7}
-        pokemonSprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
-        pokemonTypes={['water']}
-      />
+      <Container />
     </div>
   );
 }
