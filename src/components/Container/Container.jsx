1734e68187ec8e817f16574bac63d810091a874b
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-21 23:39:28 -0300

    feat: create container to request from Api and render PokeCards

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import PokeCard from '../PokemonCard';
+
+export default class Container extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allPokemons: [],
+      manyPokes: 12,
+    };
+
+    this.cardConstructor = this.cardConstructor.bind(this);
+    this.getPokemonById = this.getPokemonById.bind(this);
+    this.getMultPokes = this.getMultPokes.bind(this);
+  }
+
+  cardConstructor({ id, name, sprites, types }) {
+    return {
+      id,
+      name,
+      sprite: sprites.front_default,
+      types,
+    };
+  }
+
+  async getPokemonById(id) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await res.json();
+    return this.cardConstructor(data);
+  }
+
+  getMultPokes() {
+    const tempArr = [];
+
+    for (let i = 1; i <= this.state.manyPokes; i += 1) {
+      tempArr.push(this.getPokemonById(i));
+    }
+
+    return tempArr;
+  }
+
+  componentDidMount = () => {
+    Promise.all(this.getMultPokes()).then((data) =>
+      this.setState({ allPokemons: data })
+    );
+  };
+
+  render() {
+    const { allPokemons } = this.state;
+    return (
+      <div>
+        {allPokemons.map((item) => {
+          return (
+            <PokeCard
+              key={item.name}
+              pokemonName={item.name}
+              pokemonId={item.id}
+              pokemonSprite={item.sprite}
+              pokemonTypes={item.types}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
