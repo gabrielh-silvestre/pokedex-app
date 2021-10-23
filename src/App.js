aa4b6c97cdf36bdcb06601b975e48a6410a9b214
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-22 22:05:21 -0300

    feat: import Header to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Container from './components/Container/Container';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Container />
     </div>
   );
