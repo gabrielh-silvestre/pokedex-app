5fd9c1824aa171cceb0e495422ddf4e9550cb205
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-23 16:27:24 -0300

    style: customize Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './button.css';
 
 export default class Button extends Component {
   render() {
     const { increasePokemons } = this.props;
     return (
-      <div>
+      <div className="btn-container">
         <button onClick={increasePokemons}>{this.props.btnContent}</button>
       </div>
     );
