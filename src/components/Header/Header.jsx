b9304cff67fb3fa81f7ccb3bc690f44facc7b3ce
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-22 22:04:57 -0300

    feat: create first version of Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Turn as Hamburger } from 'hamburger-react';
+import './header.css';
+
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleMenu = this.handleMenu.bind(this);
+
+    this.state = {
+      mobileMenu: 'none',
+    };
+  }
+
+  handleMenu(toggled) {
+    toggled
+      ? this.setState({ mobileMenu: 'block' })
+      : this.setState({ mobileMenu: 'none' });
+  }
+
+  render() {
+    return (
+      <header>
+        <div className="container">
+          <div className="header-title">
+            <h1>Pokedex</h1>
+          </div>
+          <Hamburger color="#f0f0f0" size="28" onToggle={this.handleMenu} />
+          <nav className="header-menu" style={{display: this.state.mobileMenu}}>
+            <ul>
+              <li>
+                <a
+                  href="https://www.pokemon.com/br/pokedex/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Pokedex Nintendo
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/gabrielh-silvestre"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/gabrielh-silvestre/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Linkedin
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    );
+  }
+}
