886ef49a2d6fd4a7a67f5c34ebffdb5311d13d27
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-26 11:12:13 -0300

    refactor: change elements of App.js to Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Container from './components/Container/Container';
-import Header from './components/Header/Header';
+import Container from '../../components/Container/Container';
+import Header from '../../components/Header/Header';
 
 export default class Home extends Component {
   render() {
