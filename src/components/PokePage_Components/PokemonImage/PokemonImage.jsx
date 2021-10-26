13ca2e3e95c4975018f184994f2fb6c9d33cb1b3
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-26 12:32:35 -0300

    refactor: change logic place, individual component to PokemonContainer component

--- src/components/PokePage_Components/PokemonImage/PokemonImage.jsx
+++ src/components/PokePage_Components/PokemonImage/PokemonImage.jsx
@@ -1,29 +1,10 @@
 import React, { Component } from 'react'
-import fetchPokemon from '../../../API/fetchPokemon';
 
 export default class PokemonImage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      sprite: undefined,
-    }
-
-    this.getSprite = this.getSprite.bind(this);
-  }
-
-  getSprite({ sprites }) {
-    this.setState({ sprite: sprites.front_default });
-  }
-
-  componentDidMount() {
-    fetchPokemon(this.props.pokemonName, this.getSprite);
-  }
-
   render() {
     return (
       <div>
-        <img src={this.state.sprite} alt={this.props.pokemonName} />
+        <img src={this.props.sprite} alt={this.props.name} />
       </div>
     )
   }
