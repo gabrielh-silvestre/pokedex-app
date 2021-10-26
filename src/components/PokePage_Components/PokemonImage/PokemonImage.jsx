bf20469b42e2a46be3b2c55f0e7387b7e6a34625
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-26 12:06:06 -0300

    feat: implement PokemonImage component

--- src/components/PokePage_Components/PokemonImage/PokemonImage.jsx
+++ src/components/PokePage_Components/PokemonImage/PokemonImage.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
 
 export default class PokemonImage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sprite: undefined,
+    }
+
+    this.fetchPokemon = this.fetchPokemon.bind(this);
+    this.getSprite = this.getSprite.bind(this);
+  }
+
+  getSprite({ sprites }) {
+    this.setState({ sprite: sprites.front_default });
+  }
+
+  async fetchPokemon() {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.props.pokemonName}`);
+    const data = await res.json();
+    this.getSprite(data);
+  }
+
+  componentDidMount() {
+    this.fetchPokemon()
+  }
+
   render() {
     return (
       <div>
-        <img src={this.props.pokeImg} alt={this.props.pokeName} />
+        <img src={this.state.sprite} alt={this.props.pokemonName} />
       </div>
     )
   }
