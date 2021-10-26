0ff6b2db727644cf3a8d633fb1435bc792cd809a
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-26 16:14:10 -0300

    feat: create one more fetch option

---
+++ src/API/fetchAPI.js
@@ -0,0 +1,13 @@
+async function fetchPokemon(pokemon, callback) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  const data = await res.json();
+  callback(data);
+}
+
+async function fetchType(type, callback) {
+  const res = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
+  const data = await res.json();
+  callback(data);
+}
+
+export { fetchPokemon, fetchType };
