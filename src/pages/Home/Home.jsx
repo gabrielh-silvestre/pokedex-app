ce54b33f1893b13c72c24eb8f9a5b199cb4bc10f
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-26 11:04:58 -0300

    feat: create Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Container from './components/Container/Container';
+import Header from './components/Header/Header';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Container />
+      </div>
+    );
+  }
+}
