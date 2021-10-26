4d7ae4218f6abfe72c9e221bd2db57ad325b93c3
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-26 16:15:59 -0300

    chore: change file name, fetchPokemon to fetchAPI

--- src/API/fetchPokemon.js
+++
@@ -1,5 +0,0 @@
-export default async function fetchPokemon(pokemon, callback) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-  const data = await res.json();
-  callback(data);
-}
