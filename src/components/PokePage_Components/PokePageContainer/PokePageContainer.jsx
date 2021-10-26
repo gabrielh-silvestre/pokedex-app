15b781612d0d9f527b927559f9d9ff96f3d76776
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-26 12:33:11 -0300

    refactor: put the individual components logics universal

--- src/components/PokePage_Components/PokePageContainer/PokePageContainer.jsx
+++ src/components/PokePage_Components/PokePageContainer/PokePageContainer.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
 import PokemonImage from '../PokemonImage/PokemonImage'
+import PokemonName from '../PokemonName/PokemonName';
+import fetchPokemon from '../../../API/fetchPokemon';
 
 export default class PokePageContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemon: this.props.match.params.pokemonName,
+      sprite: undefined,
+      name: undefined,
+    }
+
+    this.getSprite = this.getSprite.bind(this);
+    this.getName = this.getName.bind(this);
+  }
+
+  getName({ name }) {
+    this.setState({ name: name });
+  }
+
+  getSprite({ sprites }) {
+    this.setState({ sprite: sprites.front_default });
+  }
+
+  componentDidMount() {
+    fetchPokemon(this.state.pokemon, (data) => {
+      this.getSprite(data);
+      this.getName(data);
+    });
+  }
+
   render() {
-    const { params } = this.props.match;
     return (
       <div>
-        <PokemonImage pokemonName={params.pokemonName} />
+        <PokemonImage sprite={this.state.sprite} name={this.state.name} />
+        <PokemonName name={this.state.name} />
       </div>
     )
   }
