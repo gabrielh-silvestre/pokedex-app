201d4c3e287432375bfcde37bab043a16e755ba4
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-27 21:07:48 -0300

    feat: create PokemonStats HTML structure

--- src/components/PokePage_Components/PokemonStats/PokemonStats.jsx
+++ src/components/PokePage_Components/PokemonStats/PokemonStats.jsx
@@ -2,10 +2,39 @@ import React, { Component } from 'react'
 
 export default class PokemonStats extends Component {
   render() {
+    const { attack, spAttack, defense, spDefense, speed } = this.props;
+
     return (
-      <div>
-        
-      </div>
+      <section>
+        <div>
+          <div>
+            <h3>Attack</h3>
+            <p>{attack}</p>
+          </div>
+          <div>
+            <h3>Defense</h3>
+            <p>{defense}</p>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <h3>Special-Attack</h3>
+            <p>{spAttack}</p>
+          </div>
+          <div>
+            <h3>Special-Defense</h3>
+            <p>{spDefense}</p>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <h3>Speed</h3>
+            <p>{speed}</p>
+          </div>
+        </div>
+      </section>
     )
   }
 }
