d331a3ad0e0e8fb5ad6aec1c59cbb255fe07dff8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-28 19:13:46 -0300

    feat(PokemonEvolution): create basic structure

---
+++ src/components/PokePage_Components/PokemonEvolution/PokemonEvolution.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PokemonEvolution extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
