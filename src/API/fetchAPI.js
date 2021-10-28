15178f2588472f8257ab4a8e664ee5db161f24e3
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-28 19:34:26 -0300

    revert: remove fetchEvolve

--- src/API/fetchAPI.js
+++ src/API/fetchAPI.js
@@ -10,10 +10,4 @@ async function fetchType(type, callback) {
   callback(data);
 }
 
-async function fetchEvolution(id, callback) {
-  const res = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}`);
-  const data = await res.json();
-  callback(data);
-}
-
-export { fetchPokemon, fetchType, fetchEvolution };
+export { fetchPokemon, fetchType };
