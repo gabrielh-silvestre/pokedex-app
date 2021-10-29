4e2e6d4ecbf1f4189277ecf6265ba845fda588b9
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-29 16:53:31 -0300

    style: customize header with tailwind

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,17 +21,17 @@ export default class Header extends Component {
 
   render() {
     return (
-      <header>
-        <div className="container">
-          <div className="header-title">
+      <header className="bg-red-600 py-4">
+        <div className="flex justify-between px-4 sm:container mx-auto">
+          <div className="flex items-center">
             <Link to="/">
-              <h1>Pokedex</h1>
+              <h1 className="text-4xl text-gray-100">Pokedex</h1>
             </Link>
           </div>
           <Hamburger color="#f0f0f0" size="28" onToggle={this.handleMenu} />
-          <nav className="header-menu" style={{display: this.state.mobileMenu}}>
-            <ul>
-              <li>
+          <nav className="bg-red-600 p-4 absolute right-0 top-20 rounded-bl-lg" style={{display: this.state.mobileMenu}}>
+            <ul className="text-lg text-right text-gray-100">
+              <li className="my-2">
                 <a
                   href="https://www.pokemon.com/br/pokedex/"
                   target="_blank"
@@ -40,7 +40,7 @@ export default class Header extends Component {
                   Pokedex Nintendo
                 </a>
               </li>
-              <li>
+              <li className="my-2">
                 <a
                   href="https://github.com/gabrielh-silvestre"
                   target="_blank"
@@ -49,7 +49,7 @@ export default class Header extends Component {
                   GitHub
                 </a>
               </li>
-              <li>
+              <li className="my-2">
                 <a
                   href="https://www.linkedin.com/in/gabrielh-silvestre/"
                   target="_blank"
