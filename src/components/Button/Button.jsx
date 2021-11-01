b0757a84d882c672082073d0d7b47dcf40f7ee2f
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-01 18:23:50 -0300

    refactor: change style button to be dynamic

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 
 export default class Button extends Component {
   render() {
-    const { callback, btnContent } = this.props;
+    const { callback, btnContent, className } = this.props;
     return (
       <>
-        <button className="py-2 px-4 text-lg bg-red-600 text-gray-100 font-bold rounded-md" onClick={callback}>{btnContent}</button>
+        <button className={className} onClick={callback}>{btnContent}</button>
       </>
     );
   }
