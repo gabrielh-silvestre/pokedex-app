d5b0b08692a3c2b966be156b9819007444da1561
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-02 15:30:31 -0300

    refactor: change my infinite scroll to infinite scroll with lib

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import PokeCard from '../PokemonCard/PokemonCard';
 import Button from '../Button/Button';
 import pokemonData from '../../data/PokemonData';
-
+import LoadSpinner from '../LoadSpinner/LoadSpinner';
+import InfiniteScroll from 'react-infinite-scroll-component';
 export default class Container extends Component {
   constructor(props) {
     super(props);
@@ -12,15 +13,11 @@ export default class Container extends Component {
       loading: true,
       lastPokemon: 1,
       manyPokemons: 24,
-      userPos: 0,
-      pageFinal: 2500,
-      loadMore: false,
     };
 
     this.cardConstructor = this.cardConstructor.bind(this);
     this.getPokemon = this.getPokemon.bind(this);
     this.getMultPokemons = this.getMultPokemons.bind(this);
-    this.infinniteRoll = this.infinniteRoll.bind(this);
   }
 
   cardConstructor({ id, name, sprite, types }) {
@@ -70,32 +67,33 @@ export default class Container extends Component {
     return <PokeCard key={pokemonObj.name} pokemon={pokemonObj} />;
   }
 
-  infinniteRoll({ pageY }) {
-    const { pageFinal, userPos } = this.state;
-
-    this.setState(() => ({ userPos: pageY }), () => {
-      if (userPos >= pageFinal) {
-        this.getMultPokemons();
-        this.setState({ pageFinal: pageFinal + 2500 });
-      }
-    });
-  }
-
   render() {
     const { savedPokemons } = this.state;
     return (
-      <article
+      <InfiniteScroll
+        dataLength={savedPokemons.length}
+        next={this.getMultPokemons}
+        hasMore={true}
+        loader={<LoadSpinner />}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
         className="px-4 sm:grid sm:grid-cols-2 sm:gap-4 lg:grid-cols-3 lg:px-24 xl:w-4/5 xl:mx-auto 2xl:grid-cols-4"
-        onWheel={this.infinniteRoll}
       >
         {savedPokemons
           .sort((a, b) => a.id - b.id)
           .map((pokemon) => this.renderPokeCard(pokemon))}
 
         <div className="flex justify-center pb-8 sm:col-span-full lg:hidden">
-          <Button className="py-2 px-4 text-lg bg-red-600 text-gray-100 font-bold rounded-md" btnContent="Carregar mais" callback={this.getMultPokemons} />
+          <Button
+            className="py-2 px-4 text-lg bg-red-600 text-gray-100 font-bold rounded-md"
+            btnContent="Carregar mais"
+            callback={this.getMultPokemons}
+          />
         </div>
-      </article>
+      </InfiniteScroll>
     );
   }
 }
