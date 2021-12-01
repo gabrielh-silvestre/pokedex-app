21121526e74ac10f9210389e43b4eeff29d4ab9d
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-01 19:34:46 -0300

    refactor(PokemonName): change class component to functional

--- src/components/PokemonName/PokemonName.jsx
+++ src/components/PokemonName/PokemonName.jsx
@@ -1,18 +1,9 @@
-import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
-export default class PokemonName extends Component {
-  render() {
-    const { name } = this.props;
-
-    return (
-      <>
-        {name}
-      </>
-    )
-  }
+export default function PokemonName({ name }) {
+  return name;
 }
 
 PokemonName.protoTypes = {
   name: PropTypes.string.isRequired,
-}
+};
