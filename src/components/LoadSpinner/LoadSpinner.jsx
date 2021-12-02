8fea3f7c4334f1ecda2179142bde4b3d04f8891f
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-02 11:33:38 -0300

    refactor(LoadSpinner): change class component to functional

--- src/components/LoadSpinner/LoadSpinner.jsx
+++ src/components/LoadSpinner/LoadSpinner.jsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './loadSpinner.css';
 
-export default class LoadSpinner extends Component {
-  render() {
-    return (
-      <div className="flex justify-center items-center h-full">
-        <div className="lds-ring">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
+export default function LoadSpinner() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
-    );
-  }
+    </div>
+  );
 }
