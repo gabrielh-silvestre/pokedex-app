0dc286331d16d544de883fca33f2264e2a88b4fa
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-03 12:26:25 -0300

    feat(actions): create getPokemon

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
-const receivePokemon = (newPokemon) => ({ type: 'ADD_POKEMON', newPokemon });
-
 export function addPokemon(pokemonId) {
   return async (dispatch) => {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
     const data = await res.json();
-    dispatch(receivePokemon(data));
-  }
+    dispatch({ type: 'ADD_POKEMON', newPokemon: data });
+  };
+}
+
+export function getPokemon(pokemonId) {
+  return async (dispatch) => {
+    dispatch({ type: 'REQ_POKEMON' });
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    const data = await res.json();
+    dispatch({ type: 'GET_POKEMON', pokemon: data });
+  };
 }
