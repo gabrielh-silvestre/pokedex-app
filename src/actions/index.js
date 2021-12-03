579fa0913ecf332b2a7dc28dccba1cfd573df2df
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-03 15:33:36 -0300

    feat(actions): create type actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,3 +14,28 @@ export function getPokemon(pokemonId) {
     dispatch({ type: 'GET_POKEMON', pokemon: data });
   };
 }
+
+export function getTypeRelations(typeName) {
+  return async (dispatch) => {
+    dispatch({ type: 'REQ_RELATIONS' });
+    const res = await fetch(`https://pokeapi.co/api/v2/type/${typeName}`);
+    const data = await res.json();
+    dispatch({
+      type: 'GET_DAMAGE_RELATIONS',
+      advantage: data.damage_relations.double_damage_to,
+      disadvantage: data.damage_relations.double_damage_from,
+    });
+  };
+}
+
+export function increasePokemons() {
+  return { type: 'INCREASE_POKEMONS' };
+}
+
+export function resetPokemons() {
+  return { type: 'RESET_POKEMON' };
+}
+
+export function changeRelation() {
+  return { type: 'CHANGE_RELATIONS' };
+}
