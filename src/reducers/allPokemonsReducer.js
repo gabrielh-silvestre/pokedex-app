f2525fdf946c23403c8cad64113b49cb5bb06bb5
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-02 22:16:59 -0300

    feat(allPokemonsReducer): adapt initial state

--- src/reducers/allPokemonsReducer.js
+++ src/reducers/allPokemonsReducer.js
@@ -1,9 +1,18 @@
-const INITIAL_STATE = [];
+const INITIAL_STATE = {
+  pokemons: [],
+  lastPokemon: 1,
+  manyPokemons: 24,
+};
 
 export default function allPokemons(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case 'ADD_POKEMON': 
-      return [...state, action.newPokemon];
+    case 'ADD_POKEMON':
+      return {
+        ...state,
+        pokemons: [...state.pokemons, action.newPokemon],
+        lastPokemon: state.lastPokemon + 1,
+        manyPokemons: state.manyPokemons + 1,
+      };
     default:
       return state;
   }
