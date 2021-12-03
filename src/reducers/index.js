e5e6525a35d881a53cb082ac43aba43ba6a1321d
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-03 18:33:55 -0300

    chore: remove redux directories

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-import singlePokemon from './singlePokemonReducer';
-import typesRelation from './typesReducer';
-
-const rootReducer = combineReducers({ singlePokemon, typesRelation });
-
-export default rootReducer;
