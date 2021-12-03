a50894e8a4f01f568f543f6c4cfa4ca0a55d5e00
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-03 18:29:19 -0300

    refactor(index.js): remove redux connection

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 import App from './App';
 
-import store from './store';
-
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
