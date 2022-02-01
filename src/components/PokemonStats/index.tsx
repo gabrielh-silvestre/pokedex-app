164daa95e5eb349d0d0bf8d391b4fedc84ee6e03
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-01 13:08:49 -0300

    refactor(components/PokemonStats): change JS component to TS component

---
+++ src/components/PokemonStats/index.tsx
@@ -0,0 +1,22 @@
+import { Pokemon } from 'pokenode-ts';
+import { capitalizeString } from '../../services';
+
+type PokemonStatsProps = Pick<Pokemon, 'stats'>;
+
+export function PokemonStats({ stats }: PokemonStatsProps) {
+  return (
+    <>
+      <h2 className="text-2xl text-gray-100 mb-4">Base Stats</h2>
+      <div className="lg:grid lg:grid-cols-2">
+        {stats.map(({ stat, base_stat }) => (
+          <div className="mb-2">
+            <h3 className="text-xl text-gray-100">
+              {capitalizeString(stat.name)}
+            </h3>
+            <p className="text-lg">{base_stat}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
