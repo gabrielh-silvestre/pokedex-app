055c5ef9c1a4e78e0f59cd90328db72f638647ee
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-01 13:26:03 -0300

    refactor(pages/PokemonSinglePage): change JS component to TS component

---
+++ src/pages/PokemonSinglePage/index.tsx
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { PokemonClient } from 'pokenode-ts';
+
+// import { fetchPokemon } from '../../services';
+
+import LoadSpinner from '../../components/LoadSpinner/LoadSpinner';
+import PokemonDetails from '../../components/PokemonDetails';
+
+type SinglePokemonParams = {
+  pokemonId: string;
+};
+
+export function PokemonSinglePage() {
+  const [loading, setLoading] = useState(true);
+  const [pokemon, setPokemon] = useState({});
+  const { pokemonId } = useParams<SinglePokemonParams>();
+
+  const getPokemon = useCallback(async () => {
+    const fetchPokemon = new PokemonClient();
+    setLoading(true);
+
+    const newPokemon = await fetchPokemon.getPokemonById(
+      Number.parseInt(pokemonId, 10)
+    );
+    setPokemon(newPokemon);
+
+    setLoading(false);
+  }, [pokemonId]);
+
+  useEffect(() => {
+    getPokemon();
+  }, [getPokemon]);
+
+  return (
+    <div>
+      {loading ? <LoadSpinner /> : <PokemonDetails pokemon={pokemon} />}
+    </div>
+  );
+}
