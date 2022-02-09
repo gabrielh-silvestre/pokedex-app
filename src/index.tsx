b65f23dad2d35a0da84557e03d2e65184ad79649
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-09 15:53:38 -0300

    refactor(src/): change index and App JS component to TS component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
