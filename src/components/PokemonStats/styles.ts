b7f689e1934f7364bf9ef6b578020926a73b01c5
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-09 16:32:52 -0300

    style(components/PokemonStats): separe style to specific file

---
+++ src/components/PokemonStats/styles.ts
@@ -0,0 +1,26 @@
+import tw from 'tailwind-styled-components';
+
+export const StatTitle = tw.h2`
+  mb-4
+
+  text-2xl
+  text-gray-100
+`;
+
+export const StatsContainer = tw.div`
+  lg:grid
+  lg:grid-cols-2
+`;
+
+export const ContentContainer = tw.div`
+  mb-2
+`;
+
+export const StatName = tw.h3`
+  text-xl
+  text-gray-100
+`;
+
+export const StatValue = tw.p`
+  text-lg
+`;
