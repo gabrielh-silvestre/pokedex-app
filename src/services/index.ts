466435e98c92e480b6eb97409a3d544aaab3a25f
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-09 23:25:16 -0300

    refactor(src/services): change default fetch for PokemonClient

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,14 +1,9 @@
-export async function fetchPokemon(pokemon: number) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-  const data = await res.json();
-  return data;
-}
+import { PokemonClient } from 'pokenode-ts';
 
-export async function fetchType(type: string) {
-  const res = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
-  const data = await res.json();
-  return data;
-}
+export const fetchPokemon = new PokemonClient();
+
+export const fetchType = (type: string) => fetchPokemon.getTypeByName(type);
+
 
 export function capitalizeString(str: string) {
   return str.replace(/^\w/, (char) => char.toUpperCase());
