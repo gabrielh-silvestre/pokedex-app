87180a899b29df04deab0eec37a8696f383f6018
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-12 11:46:13 -0300

    feat(services/localStorage): create localStorage services
    - Local storage and Redux store are set with the same action

---
+++ src/services/localStorage.ts
@@ -0,0 +1,41 @@
+import { Pokemon } from 'pokenode-ts';
+
+const LOCAL_FAVORITES = 'favoritePokemons';
+
+function getInitialValue(
+  key: string,
+  defaultValue: any,
+  convertFromString = JSON.parse
+) {
+  const localStorageValue = localStorage.getItem(key);
+  if (localStorageValue) {
+    try {
+      return convertFromString(localStorageValue);
+    } catch {
+      localStorage.removeItem(key);
+    }
+  }
+  return defaultValue;
+}
+
+export async function addItemToFavoriteLocal(
+  newItem: Pokemon,
+  convertToString = JSON.stringify
+) {
+  const favoriteItems = getInitialValue(LOCAL_FAVORITES, []);
+  const attItems = [...favoriteItems, newItem];
+
+  localStorage.setItem(LOCAL_FAVORITES, convertToString(attItems));
+}
+
+export async function removeFromFavoriteLocal(
+  removeItem: Pokemon,
+  convertToString = JSON.stringify
+) {
+  const favoriteItems = getInitialValue(LOCAL_FAVORITES, []);
+  const attItems = favoriteItems.filter(
+    (item: Pokemon) => item.id !== removeItem.id
+  );
+
+  localStorage.setItem(LOCAL_FAVORITES, convertToString(attItems));
+}
