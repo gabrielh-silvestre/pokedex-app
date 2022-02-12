cd0ebfc42edb85cc81a76f37bd691dd5ee900df3
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-12 12:57:21 -0300

    style(pages/Favorites): separe style to specific file

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -4,20 +4,22 @@ import { RootState } from '../../redux/store';
 
 import { PokemonCard } from '../../components/PokemonCard';
 
+import { Container, FavoriteAlert } from './styles';
+
 export function Favorites() {
   const favorites = useSelector((state: RootState) => state.favorite);
 
   return (
-    <main className="relative container h-full md:grid md:grid-cols-2 md:gap-x-8 lg:grid-cols-3 2xl:grid-cols-4">
+    <Container>
       {favorites.length > 0 ? (
         favorites.map((pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} />
         ))
       ) : (
-        <div className="h-full flex justify-center items-center text-2xl text-center text-gray-500 md:col-span-full">
+        <FavoriteAlert>
           Sorry you do not have any favorite Pokemon :(
-        </div>
+        </FavoriteAlert>
       )}
-    </main>
+    </Container>
   );
 }
