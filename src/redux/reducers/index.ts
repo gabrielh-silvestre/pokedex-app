dc8b18fd64e107533848e02962b424d2657b8385
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-12 18:37:51 -0300

    feat(reducers/index.ts): combine PokemonReducer in rootReducer

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
 import { FavoriteReducer } from "./favoriteReducer";
+import { PokemonReducer } from './pokemonReducer';
 
 export const rootReducer = combineReducers({
   favorite: FavoriteReducer,
+  pokemons: PokemonReducer,
 });
