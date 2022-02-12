1a1c31a9f5d2db4721cc85f01febc0dbf2929b29
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-12 17:09:41 -0300

    refactor(services/): put api functions in a specific file
    - Index now contains general utility

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,19 +1,3 @@
-import { PokemonClient } from 'pokenode-ts';
-
-export const fetchPokemon = new PokemonClient();
-
-export const fetchType = (type: string) => fetchPokemon.getTypeByName(type);
-
-
 export function capitalizeString(str: string) {
   return str.replace(/^\w/, (char) => char.toUpperCase());
-}
-
-export async function getDmgRelations(type: string) {
-  const data = await fetchType(type);
-
-  return {
-    advantage: data.damage_relations.double_damage_to,
-    disadvantage: data.damage_relations.double_damage_from,
-  };
 }
