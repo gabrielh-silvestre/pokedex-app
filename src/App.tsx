672be508a89411d42c6c22da62c838fa8fbf0624
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-12 12:53:13 -0300

    style(src/App.tsx): manually define height

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { Route, Switch } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { PokemonSinglePage } from './pages/PokemonSinglePage';
+import { Favorites } from './pages/Favorites';
 import { Header } from './components/Header';
 
 function App() {
   return (
-    <div>
+    <div className="h-screen">
       <Header />
 
       <Switch>
         <Route
-          path="/pokemon/:pokemonId" component={ PokemonSinglePage } />
+          path="/pokemon/:pokemonId"
+          component={ PokemonSinglePage }
+        />
+        <Route path="/favorites" component={ Favorites } />
         <Route path="/" component={Home} />
       </Switch>
     </div>
