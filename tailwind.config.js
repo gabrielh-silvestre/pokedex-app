4b887120d336650e23c11164efc56e8f84a8888e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-12 12:52:16 -0300

    build(tailwind): config default container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,20 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
