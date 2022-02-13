128fa8f570114da49d471d06727fca7582d6052d
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-13 13:20:29 -0300

    feat(Header/SubHeader): fetch pokemons by type or generation

--- src/components/Header/SubHeader/index.tsx
+++ src/components/Header/SubHeader/index.tsx
@@ -1,11 +1,15 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import {
   fetchGenerationList,
   fetchTypesList,
 } from '../../../redux/actions/searchOptionsActions';
+import {
+  getPokemonsByGeneration,
+  getPokemonsByType,
+} from '../../../redux/actions/pokemonActions';
 import { RootState } from '../../../redux/store';
 import { capitalizeString } from '../../../services';
 
@@ -15,6 +19,18 @@ export function SubHeader() {
     (state: RootState) => state.searchOptions
   );
 
+  const setPokemonsGeneration = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(getPokemonsByGeneration(value));
+  };
+
+  const setPokemonsType = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(getPokemonsByType(value));
+  };
+
   useEffect(() => {
     dispatch(fetchGenerationList());
     dispatch(fetchTypesList());
@@ -22,16 +38,18 @@ export function SubHeader() {
 
   return (
     <nav>
-      <select>
+      <select onChange={setPokemonsGeneration}>
         {generations.map(({ name }, i) => (
           <option value={name}>{`${i + 1}º Generation`}</option>
         ))}
       </select>
-      <select>
-        {types.map(({ name }) => (
-          name !== 'unknown'
-          && <option value={name}>{capitalizeString(name)}</option>
-        ))}
+      <select onChange={setPokemonsType}>
+        {types.map(
+          ({ name }) =>
+            name !== 'unknown' && (
+              <option value={name}>{capitalizeString(name)}</option>
+            )
+        )}
       </select>
       <Link to="/favorites">Favorites</Link>
       <div>Bag</div>
