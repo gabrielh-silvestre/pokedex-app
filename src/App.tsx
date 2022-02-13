77c7ab019164220db958c592e0bcc41a55fc310f
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-13 19:22:35 -0300

    feat(src/App.tsx): create route for StaticList

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
 import { Home } from './pages/Home';
+import { StaticList } from './pages/StaticList';
 import { PokemonSinglePage } from './pages/PokemonSinglePage';
 import { Favorites } from './pages/Favorites';
 import { Header } from './components/Header/MainHeader';
@@ -16,6 +17,7 @@ function App() {
           path="/pokemon/:pokemonId"
           component={ PokemonSinglePage }
         />
+        <Route path="/searchBy/:searchOpt" component={ StaticList } />
         <Route path="/favorites" component={ Favorites } />
         <Route path="/" component={Home} />
       </Switch>
